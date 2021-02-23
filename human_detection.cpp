1d0fe6b76304ba79ad858a5d9aa04f759554815a
MagnusBC98 <magnusbc98@gmail.com>
2021-02-23 17:44:50 +0000

    Using YOLO networks instead of Haar cascades

--- human_detection.cpp
+++ human_detection.cpp
@@ -1,112 +1,102 @@
-#if defined(__linux__) || defined(LINUX) || defined(__APPLE__) || defined(ANDROID) || (defined(_MSC_VER) && _MSC_VER>=1800)
-
+#include <iostream>
 #include <opencv2/opencv.hpp>
-#include <opencv2/imgproc.hpp>  // Gaussian Blur
-#include <opencv2/core.hpp>        // Basic OpenCV structures (cv::Mat, Scalar)
+#include <opencv2/highgui.hpp>
+#include <opencv2/video.hpp>
+#include <opencv2/dnn.hpp>
 #include <opencv2/videoio.hpp>
-#include <opencv2/highgui.hpp>  // OpenCV window I/O
-#include <opencv2/features2d.hpp>
-#include <opencv2/objdetect.hpp>
-
-#include <stdio.h>
-
-using namespace std;
+#include <opencv2/imgproc.hpp>
 using namespace cv;
+using namespace std;
+using namespace dnn;
 
-const string WindowName = "Face Detection example";
-
-class CascadeDetectorAdapter: public DetectionBasedTracker::IDetector
-{
+class HumanDetector{
     public:
-        CascadeDetectorAdapter(Ptr<CascadeClassifier> detector):
-            IDetector(),
-            Detector(detector)
-        {
-            CV_Assert(detector);
+        // prepare model files and set up network
+        std::string model = "yolov2-tiny.weights";
+        std::string config = "yolov2-tiny.cfg";        
+        Net network = readNet(model, config,"Darknet"); 
+
+        void configure_network(){
+            network.setPreferableBackend(DNN_BACKEND_DEFAULT);
+            network.setPreferableTarget(DNN_TARGET_OPENCL);
         }
 
-        void detect(const cv::Mat &Image, std::vector<cv::Rect> &objects) CV_OVERRIDE
-        {
-            Detector->detectMultiScale(Image, objects, scaleFactor, minNeighbours, 0, minObjSize, maxObjSize);
+        void detect(Mat img){
+            static Mat blobFromImg;
+            bool swapRB = true;
+            // function which facilitates classification
+            blobFromImage(img, blobFromImg, 1, Size(416, 416), Scalar(), swapRB, false);
+            //cout << blobFromImg.size() << endl; 
+            
+            float scale = 1.0 / 255.0;
+            Scalar mean = 0;
+            network.setInput(blobFromImg, "", scale, mean);
+
+            Mat outMat;
+            network.forward(outMat);
+            // number of detected objects
+            int rowsNoOfDetection = outMat.rows;
+            int colsCoordinatesPlusClassScore = outMat.cols;
+            // Loop over number of detected objects
+            for (int j = 0; j < rowsNoOfDetection; ++j)
+            {
+                // get classifier scores
+                Mat scores = outMat.row(j).colRange(5, colsCoordinatesPlusClassScore);
+                Point PositionOfMax;
+                double confidence;
+
+                minMaxLoc(scores, 0, &confidence, 0, &PositionOfMax);
+                if (confidence > 0.0001)
+                {
+                    // locate object
+                    int centerX = (int)(outMat.at<float>(j, 0) * img.cols); 
+                    int centerY = (int)(outMat.at<float>(j, 1) * img.rows); 
+                    int width =   (int)(outMat.at<float>(j, 2) * img.cols+20); 
+                    int height =   (int)(outMat.at<float>(j, 3) * img.rows+100); 
+                    int left = centerX - width / 2;
+                    int top = centerY - height / 2;
+
+                    stringstream ss;
+                    ss << PositionOfMax.x;
+                    string clas = ss.str();
+                    putText(img, "Human Detected", Point(left, top), 1, 2, Scalar(0, 0, 255), 2, false);
+                    // draw rectangle around object
+                    rectangle(img, Rect(left, top, width, height), Scalar(0,0,255), 2, 8, 0);
+                }
+            }     
         }
+};
 
-        virtual ~CascadeDetectorAdapter() CV_OVERRIDE
-        {}
+class Camera{
+  public:
+      Mat img;
+      HumanDetector mydetector;
 
-    private:
-        CascadeDetectorAdapter();
-        Ptr<CascadeClassifier> Detector;
- };
+      // start camera method
+      int start(){
+            // initialise capture
+            mydetector.configure_network();
+            VideoCapture cap(0);   
+            namedWindow("Display window", WINDOW_NORMAL);
+            while(1){
+               cap >> img;
+               if (!cap.isOpened()) {
+               cout << "Video Capture Fail" << endl;
+               break;
+            }
+               mydetector.detect(img);
+               imshow("Display window", img);
+               if (waitKey(10) == 27){       
+                  break;
+               }   
+            } 
+          return 0;
+      }                   
+};
 
-int main(int , char** )
-{
-    namedWindow(WindowName);
-
-    VideoCapture VideoStream(0);
-
-    if (!VideoStream.isOpened())
-    {
-        printf("Error: Cannot open video stream from camera\n");
-        return 1;
-    }
-
-    string cascadeFrontalfilename = "haarcascade_upperbody.xml";
-    Ptr<CascadeClassifier> cascade = makePtr<CascadeClassifier>(cascadeFrontalfilename);
-    Ptr<DetectionBasedTracker::IDetector> MainDetector = makePtr<CascadeDetectorAdapter>(cascade);
-    if ( cascade->empty() )
-    {
-      printf("Error: Cannot load %s\n", cascadeFrontalfilename.c_str());
-      return 2;
-    }
-
-    cascade = makePtr<CascadeClassifier>(cascadeFrontalfilename);
-    Ptr<DetectionBasedTracker::IDetector> TrackingDetector = makePtr<CascadeDetectorAdapter>(cascade);
-    if ( cascade->empty() )
-    {
-      printf("Error: Cannot load %s\n", cascadeFrontalfilename.c_str());
-      return 2;
-    }
-
-    DetectionBasedTracker::Parameters params;
-    DetectionBasedTracker Detector(MainDetector, TrackingDetector, params);
-
-    if (!Detector.run())
-    {
-        printf("Error: Detector initialization failed\n");
-        return 2;
-    }
-
-    Mat ReferenceFrame;
-    Mat GrayFrame;
-    vector<Rect> Faces;
-
-    do
-    {
-        VideoStream >> ReferenceFrame;
-        cvtColor(ReferenceFrame, GrayFrame, COLOR_BGR2GRAY);
-        Detector.process(GrayFrame);
-        Detector.getObjects(Faces);
-
-        for (size_t i = 0; i < Faces.size(); i++)
-        {
-            rectangle(ReferenceFrame, Faces[i], Scalar(0,255,0));
-        }
-
-        imshow(WindowName, ReferenceFrame);
-    } while (waitKey(30) < 0);
-
-    Detector.stop();
-
-    return 0;
-}
-
-#else
-
-#include <stdio.h>
 int main()
 {
-    printf("This sample works for UNIX or ANDROID or Visual Studio 2013+ only\n");
+    Camera mycamera;
+    mycamera.start();
     return 0;
 }
-
-#endif
