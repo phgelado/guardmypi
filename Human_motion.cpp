8ab112cac3ca30c49ed74777397cbd52d42d2619
Aidan <aidan.porteous98@gmail.com>
2021-02-23 16:34:15 +0000

    Removed red rectangle from frame around motion

--- Human_motion.cpp
+++ Human_motion.cpp
@@ -135,7 +135,7 @@ Mat MotionDetector::ProcessContours(Mat camerafeed) {
         if(contourArea(cnts[i]) < 5000) 
             continue;
             		
-			cout << "Motion Detected - Initiate next thread";			
+			cout<<"Motion Detected";			
 		}
 		
 	return camerafeed;
