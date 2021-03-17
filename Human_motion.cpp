828c32617b0207899ba86447a2e1994eebaaa889
Aidan <aidan.porteous98@gmail.com>
2021-03-17 12:18:46 +0000

    New Human Detection integrated with threading

--- Human_motion.cpp
+++ Human_motion.cpp
@@ -5,6 +5,7 @@
 */
 
 #include <opencv2/core/utility.hpp>
+#include "opencv2/objdetect/objdetect.hpp"
 #include <opencv2/tracking.hpp>
 #include <opencv2/videoio.hpp>
 #include <opencv2/highgui.hpp>
@@ -12,12 +13,10 @@
 #include <cstring>
 #include <unistd.h>
 #include <thread>
-#include <opencv2/dnn.hpp>
-#define min_area 500 //pixels
 
+#define min_area 500 //pixels
 using namespace std;
 using namespace cv;
-using namespace dnn;
 
 
 
@@ -56,76 +55,36 @@ class MotionDetector {
 
 class HumanDetector{
     public:
-        // prepare model files and set up network
-        std::string model = "yolov2-tiny.weights";
-        std::string config = "yolov2-tiny.cfg";        
-        Net network = readNet(model, config,"Darknet"); 
-        int human_flag;
-        int pet_flag;
+        // cascade classifier object
+        CascadeClassifier human_cascade;
+        // grayscale frame for processing
+        Mat GrayFrame;
 
-        void configure_network(){
-            network.setPreferableBackend(DNN_BACKEND_DEFAULT);
-            network.setPreferableTarget(DNN_TARGET_OPENCL);
+        // method for loading particular Haar cascade file
+        int loadcascade(){
+            human_cascade.load("haarcascade_upperbody.xml");
+            if(!human_cascade.load("haarcascade_upperbody.xml"))
+           {
+                cerr<<"Error Loading XML file"<<endl;
+            return 0;
+           }    
         }
 
-        void detect(Mat img){
-            static Mat blobFromImg;
-            bool swapRB = true;
-            // function which facilitates classification
-            blobFromImage(img, blobFromImg, 1, Size(416, 416), Scalar(), swapRB, false);
-            //cout << blobFromImg.size() << endl; 
-            
-            float scale = 1.0 / 255.0;
-            Scalar mean = 0;
-            network.setInput(blobFromImg, "", scale, mean);
-
-            Mat outMat;
-            network.forward(outMat);
-            // number of detected objects
-            int rowsNoOfDetection = outMat.rows;
-            int colsCoordinatesPlusClassScore = outMat.cols;
-            // Loop over number of detected objects
-            for (int j = 0; j < rowsNoOfDetection; ++j)
+        // method for detecting humans in frame
+        void detect(Mat ReferenceFrame){
+            // store original frame as grayscale in gray frame
+            cvtColor(ReferenceFrame, GrayFrame, COLOR_BGR2GRAY);
+            std::vector<Rect> humans;
+            // detect humans in frame - adjust parameters as desired
+            human_cascade.detectMultiScale( GrayFrame, humans, 1.5, 2);     
+  
+            // Draw rectangles on the detected humans
+            for( int i = 0; i < humans.size(); i++ )
             {
-                // get classifier scores
-                Mat scores = outMat.row(j).colRange(5, colsCoordinatesPlusClassScore);
-                Point PositionOfMax;
-                double confidence;
-
-                minMaxLoc(scores, 0, &confidence, 0, &PositionOfMax);
-                if (confidence > 0.0001)
-                {
-                    // locate object
-                    stringstream ss;
-                    ss << PositionOfMax.x;
-                    string clas = ss.str();
-                    if ((clas =="0") || (clas=="16")){
-                        int centerX = (int)(outMat.at<float>(j, 0) * img.cols); 
-                        int centerY = (int)(outMat.at<float>(j, 1) * img.rows); 
-                        int width =   (int)(outMat.at<float>(j, 2) * img.cols+20); 
-                        int height =   (int)(outMat.at<float>(j, 3) * img.rows+100); 
-                        int left = centerX - width / 2;
-                        int top = centerY - height / 2;
-                        
-                        if (clas =="0"){
-                            putText(img, "Human Detected", Point(left, top), 1, 2, Scalar(0, 255, 255), 2, false);
-                            human_flag = 1;
-                        }
-                        else if (clas =="16"){
-                            putText(img, "Dog Detected", Point(left, top), 1, 2, Scalar(0, 255, 255), 2, false);
-                            pet_flag = 1;
-                        }
-                        stringstream ss2;
-                        ss << confidence;
-                        string conf = ss.str();
-                        // draw rectangle around object
-                        rectangle(img, Rect(left, top, width, height), Scalar(0,0,255), 2, 8, 0);
-                    }
-                }
-            }     
+             rectangle(ReferenceFrame, humans[i], Scalar(0,255,0));
+            } 
         }
 };
-
 
 /**
 * @class Camera
@@ -154,6 +113,7 @@ class Camera {
 		//Open the video feed for the webcam/camera
 		video.open(0);
 		sleep(3);
+		Hdetector.loadcascade();
 
         // Check that video is opened
 	    if (!video.isOpened()) return -1;
