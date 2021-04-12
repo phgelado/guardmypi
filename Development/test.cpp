14953e6396660553f34cefe2b35f24f2b34dc902
Aidan <aidan.porteous98@gmail.com>
2021-04-12 14:39:20 +0100

    Test module added to check for the test.jpeg being made in the streaming segement

--- Development/test.cpp
+++ Development/test.cpp
@@ -1,5 +1,6 @@
 #define BOOST_TEST_MODULE SqrTests
 #include <boost/test/unit_test.hpp>
+#include <boost/filesystem.hpp>
 #include <opencv2/core/utility.hpp>
 #include "opencv2/objdetect/objdetect.hpp"
 #include <opencv2/tracking.hpp>
@@ -18,9 +19,11 @@ Camera Cam;
 BOOST_AUTO_TEST_CASE(FailTest)
 {
     BOOST_CHECK_EQUAL(-1, Cam.opencam(1));
+    BOOST_CHECK(!boost::filesystem::exists("/home/aidan/repos/guardmypi/test.jpeg"));
 }
 
 BOOST_AUTO_TEST_CASE(PassTest)
 {
     BOOST_CHECK_EQUAL(0,Cam.opencam(0));
+    BOOST_CHECK(boost::filesystem::exists("/home/aidan/repos/guardmypi/test.jpeg"));
 }
